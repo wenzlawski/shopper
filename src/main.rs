db345760701817bfa57dba80fa3da4c60161fd29
Marc Wenzlawski <llbbbmn.wenzlawski@gmail.com>
2020-05-07 19:32:04 +0000

    fixed shop ownership problem and added inventory query

--- src/main.rs
+++ src/main.rs
@@ -19,51 +19,53 @@ mod tests {
     }
 
     #[test]
-    fn shopper_buy_basket() {
-        //let item1 = Item::_new(String::from("Bread"), 6.5, 5, 2);
-        //let item2 = Item::_new(String::from("Jeans"), 63.0, 8, 6);
-        //let inv = vec![item1, item2];
-        //let mut shopper = Shopper::_new(5000.0, 100);
+    fn find_items_in_shop() {
+        let shop = _create_generic_shop();
+        let found: Vec<&Item> = shop._get_item_by_name("Jeans");
 
-        //match shopper._buy(inv) {
-        //Ok(_) => println!("Purchase successful!"),
-        //Err(e) => println!("{}", e),
-        //}
-
-        //assert_eq!(shopper.inventory.len(), 2);
-        //assert_eq!(shopper.capacity, 42);
-        //assert_eq!(shopper.money, 4609.0);
+        assert_eq!(found[0].name, "Jeans");
+        assert_eq!(found.len(), 2);
     }
 
     #[test]
-    fn shopper_buy_from_shop() {
-        //let shop = _create_generic_shop();
-        //let mut shopper = Shopper::_new(5000.0, 100);
+    fn go_into_shop_and_buy() {
+        let mut mall = _create_generic_mall();
+        let shopper = Shopper::_new(1000.0, 300);
 
-        //match shopper._buy(inv) {
-        //Ok(_) => println!("Purchase successful!"),
-        //Err(e) => println!("{}", e),
-        //}
+        let mut shopper = shopper
+            ._go_into_shop(String::from("Best Shop ever"), &mut mall)
+            .unwrap();
 
-        //assert_eq!(shopper.inventory.len(), 2);
-        //assert_eq!(shopper.capacity, 42);
-        //assert_eq!(shopper.money, 4609.0);
+        shopper._take_item(String::from("Jeans"), 3).unwrap();
+        shopper._take_item(String::from("Necklace"), 1).unwrap();
+
+        shopper._buy_basket().unwrap();
+
+        assert_eq!(shopper.inventory[0].name, String::from("Jeans"));
+        assert_eq!(shopper.inventory.len(), 2);
+        assert_eq!(shopper.money, 781.0);
+        assert_eq!(shopper.capacity, 235);
+        //assert_eq!(shopper.cur)
     }
-
-    #[test]
-    fn take_items_in_shop() {}
 }
 
 fn _create_generic_shop() -> Shop {
     let item1 = Item::_new(String::from("Bread"), 6.5, 5, 20);
     let item2 = Item::_new(String::from("Jeans"), 63.0, 20, 9);
+    let item21 = Item::_new(String::from("Jeans"), 63.0, 20, 9);
     let item3 = Item::_new(String::from("Necklace"), 30.0, 5, 6);
     let item4 = Item::_new(String::from("Painting"), 200.0, 50, 2);
     let item5 = Item::_new(String::from("Gold"), 2000.0, 80, 0);
-    let inv = vec![item1, item2, item3, item4, item5];
+    let inv = vec![item1, item2, item21, item3, item4, item5];
     Shop::_new(String::from("Best Shop ever"), inv)
 }
 
+fn _create_generic_mall() -> Mall {
+    let shop1 = _create_generic_shop();
+    let shop2 = _create_generic_shop();
+    let shops = vec![shop1, shop2];
+    Mall::_new(shops)
+}
 /*
    Store:
     ** when 'in' store have extra carrying capacity.
@@ -86,11 +88,11 @@ impl Mall {
         Mall { shops }
     }
 
-    fn get_shops(&mut self) -> &Vec<Shop> {
+    fn _get_shops(&mut self) -> &Vec<Shop> {
         &self.shops
     }
 
-    fn take_shop(&mut self, name: String) -> Result<Shop, &str> {
+    fn _take_shop(&mut self, name: String) -> Result<Shop, &str> {
         for (index, shop) in &mut self.shops.iter().enumerate() {
             if shop.name == name {
                 return Ok(self.shops.remove(index));
@@ -99,7 +101,7 @@ impl Mall {
         Err("shop not found.")
     }
 
-    fn give_shop(&mut self, shop: Shop) {
+    fn _give_shop(&mut self, shop: Shop) {
         self.shops.push(shop);
     }
 }
@@ -110,7 +112,6 @@ struct Shopper {
     money: f32,
     capacity: u32,
     inventory: Vec<Item>,
-    current_location: Option<Shop>,
 }
 
 impl Shopper {
@@ -119,34 +120,41 @@ impl Shopper {
             money,
             capacity,
             inventory: vec![],
-            current_location: None,
         }
     }
 
     // enter shop - this changes trait to InShop
-    //fn go_into_shop(
-    //&mut self,
-    //shop_name: String,
-    //mall: &mut Mall,
-    //) -> Result<ShopperInShop, &str> {
-    //match mall.take_shop(shop_name) {
-    //Ok(s) => {
-    //Ok(ShopperInShop {
-    //money: self.money,
-    //capacity: self.capacity,
-    //inventory: self.inventory,
-    //current_location: Some(s),
-    //})
-    //}
-    //Err(e) => return Err(e),
-    //}
-    //Ok(ShopperInShop {
-    //money: self.money,
-    //capacity: self.capacity,
-    //inventory: self.inventory,
-    //current_location: Some(shop),
-    //})
-    //}
+    fn _go_into_shop<'a>(
+        self,
+        shop_name: String,
+        mall: &'a mut Mall,
+    ) -> Result<ShopperInShop, &str> {
+        // Problem here is possibly that the value we get from mall doesn't
+        // live as long as the returned ShopperInShop;
+        // Possible solution would be to work with lifetime annotations or
+        // rethink how the ShopperInShop and its relation to Shop is handeled.
+        let shop = mall._take_shop(shop_name);
+
+        let res = match shop {
+            Ok(s) => Ok(ShopperInShop {
+                money: self.money,
+                capacity: self.capacity,
+                inventory: self.inventory,
+                basket: vec![],
+                current_location: s,
+            }),
+            Err(e) => Err(e),
+        };
+        res
+
+        //Ok(ShopperInShop {
+        //money: self.money,
+        //capacity: self.capacity,
+        //inventory: self.inventory,
+        //basket: vec![],
+        //current_location: shop,
+        //})
+    }
 
     // exit the game and display end statistics
     //fn go_home(&self) {}
@@ -156,16 +164,17 @@ struct ShopperInShop {
     money: f32,
     capacity: u32,
     inventory: Vec<Item>,
-    current_location: Option<Shop>,
+    basket: Vec<Item>,
+    current_location: Shop,
 }
 
 impl ShopperInShop {
-    fn _buy(&mut self, basket: Vec<Item>) -> Result<(), &str> {
+    fn _buy_basket(&mut self) -> Result<(), &str> {
         let mut tot_cost: f32 = 0.0;
         let mut tot_size: u32 = 0;
 
         // shorten this with map?
-        for b in &basket {
+        for b in &self.basket {
             tot_cost += b.quantity as f32 * b.cost;
             tot_size += b.quantity * b.size;
         }
@@ -179,23 +188,28 @@ impl ShopperInShop {
         self.money -= tot_cost;
         self.capacity -= tot_size;
 
-        self._update_inventory(basket);
+        self._update_inventory();
 
         Ok(())
     }
 
-    fn _update_inventory(&mut self, basket: Vec<Item>) -> &Vec<Item> {
-        let mut addn = vec![];
-        for b in basket {
+    fn _take_item(&mut self, item: String, quantity: u32) -> Result<(), &str> {
+        let item = self.current_location._take_item(item, quantity)?;
+        self.basket.push(item);
+        Ok(())
+    }
+
+    fn _update_inventory(&mut self) -> &Vec<Item> {
+        for b in &mut self.basket[..].iter() {
             for i in &mut self.inventory {
                 if b.name == i.name {
                     i.quantity += b.quantity;
                     break;
                 }
             }
-            addn.push(b)
+            self.inventory.push(b.clone());
         }
-        self.inventory.extend(addn);
+        self.basket = vec![];
         &self.inventory
     }
     // Add item amount to basket and remove from shop inventory
@@ -231,7 +245,19 @@ impl Shop {
         Shop { name, inventory }
     }
 
-    fn take_item(&mut self, item: String, quantity: u32) -> Result<Item, &str> {
+    fn _get_inventory(&self) -> &Vec<Item> {
+        &self.inventory
+    }
+
+    fn _get_item_by_name(&self, name: &str) -> Vec<&Item> {
+        self.inventory.iter().filter(|x| x.name == name).collect()
+    }
+
+    fn _take_item(
+        &mut self,
+        item: String,
+        quantity: u32,
+    ) -> Result<Item, &str> {
         for i in &mut self.inventory {
             if item == i.name {
                 if quantity <= i.quantity {
@@ -245,14 +271,12 @@ impl Shop {
                 } else {
                     return Err("Not enough of the item left.");
                 }
-            } else {
-                return Err("Item not found in shop.");
             }
         }
-        Err("Something went wrong")
+        Err("Item not found in shop.")
     }
 
-    fn put_item_back(&mut self, item: Item) {
+    fn _put_item_back(&mut self, item: Item) {
         for i in &mut self.inventory {
             if item.name == i.name {
                 i.quantity += item.quantity;
@@ -262,7 +286,7 @@ impl Shop {
     }
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 struct Item {
     name: String,
     cost: f32,
@@ -281,19 +305,4 @@ impl Item {
     }
 }
 
-fn main() {
-    // Functions needed:
-
-    //let money: f32 = 100.0;
-    //let capacity: u32 = 100;
-
-    //let shopper = Shopper::_new(money, capacity);
-    //println!("{:?}", shopper);
-
-    //let cost: f32 = 30.0;
-    //let size: u32 = 30;
-
-    //let item = Item::_new("Cho"cost, size);
-    //println!("{:?}", item);
-}
-//println!("Test");
+fn main() {}
